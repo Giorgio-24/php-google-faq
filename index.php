c4950718785ace3394ae279d35df71ea27d95f57
Giorgio Andrei <cipuandrei@libero.it>
2021-09-22 16:26:19 +0200

    bootstrap-added

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/css/bootstrap.css' integrity='sha512-YfFXNd2o6swxA1M0ll6EDdnVdYdE6iz+C6k0Guqf18JW6sVq6Oz9lfbjOso+LMwwNYNxUbp7egkYmC2W/IyeVA==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="./style.scss">
+
     <title>Google faq</title>
 </head>
 
